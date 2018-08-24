8bb37da60ccf50306a684caa43a2bf6efa2c0f92
Ethan <a8843526@gmail.com>
2018-08-24 16:15:48 +0800

    add cmdline

--- main.go
+++ main.go
@@ -1,25 +1,18 @@
 package main
 
 import (
-	"driver/links"
-	"sync"
-
+	"cmdline"
 	"driver/category"
-	"flag"
+	"driver/links"
 	"schema"
 	"ssdb"
+	"sync"
 
-	"github.com/BurntSushi/toml"
 	"github.com/golang/glog"
 )
 
 func main() {
-	configPath := flag.String("config", "conf/dev.toml", "specific config file")
-	flag.Parse()
-
-	if _, err := toml.DecodeFile(*configPath, &schema.Config); err != nil {
-		glog.Fatalf("Parser config error : %+v", err)
-	}
+	cmd.InitCmd()
 
 	defer glog.Flush()
 	glog.Info("Spider GO!")
---
+++ src/cmdline/init_cmd.go
@@ -0,0 +1,17 @@
+package cmd
+
+import (
+	"flag"
+	"schema"
+
+	"github.com/BurntSushi/toml"
+	"github.com/golang/glog"
+)
+
+func InitCmd() {
+	configPath := flag.String("config", "conf/dev.toml", "specific config file")
+	if _, err := toml.DecodeFile(*configPath, &schema.Config); err != nil {
+		glog.Fatalf("Parser config error : %+v", err)
+	}
+	flag.Parse()
+}
